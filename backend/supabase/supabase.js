bcf27a468c421ddf2d5032c76b7ae318e996c915
Alexis Fuenmayor <afuenmayor1316@gmail.com>
2026-07-16 14:35:14 -0500

    Added forgot Password

--- backend/supabase/supabase.js
+++ backend/supabase/supabase.js
@@ -1,11 +1,16 @@
 import {createClient} from "@supabase/supabase-js"
 import "dotenv/config"
+import ws from "ws"
 
 const SUPABASE_URL = process.env.VITE_SUPABASE_URL
 const SUPABASE_KEY =
     process.env.SUPABASE_SERVICE_ROLE_KEY ||
     process.env.VITE_SUPABASE_PUBLISHABLE_KEY
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
+    realtime:{
+        transport:ws
+    }
+})
 
 export default supabase
